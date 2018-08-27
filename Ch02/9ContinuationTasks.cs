cffef594cd11bf4dc80d0b9c7caee6b8e55039dd
shakti singh tanwar <shakti_stanwar@hotmail.com>
2018-08-27 18:34:55 +0530

    Chapter 3 and 4
    Chapter 3 and 4

--- Ch02/9ContinuationTasks.cs
+++ Ch02/9ContinuationTasks.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Ch02
 {
@@ -14,6 +15,31 @@ namespace Ch02
             SimpleChain();
             Console.ReadLine();
             //PartialResponsive();
+            ContinueWhenAll();
+            ContinueWhenAny();
+        }
+
+        private static void ContinueWhenAny()
+        {
+            int number = 13;
+            Task<bool> taskA = Task.Factory.StartNew<bool>(() => number / 2 != 0);
+            Task<bool> taskB = Task.Factory.StartNew<bool>(() => (number / 2) * 2 != number);
+            Task<bool> taskC = Task.Factory.StartNew<bool>(() => (number & 1) != 0);
+            Task.Factory.ContinueWhenAny<bool>(new Task<bool>[] { taskA, taskB, taskC }, (task) =>
+            {
+                Console.WriteLine((task as Task<bool>).Result);
+            }
+          );        
+        }
+
+        private async static void ContinueWhenAll()
+        {
+            int a = 2, b = 3;
+            Task<int> taskA = Task.Factory.StartNew<int>(() => a * a);
+            Task<int> taskB = Task.Factory.StartNew<int>(() => b * b);
+            Task<int> taskC = Task.Factory.StartNew<int>(() => 2 * a * b);
+            var sum = await Task.Factory.ContinueWhenAll<int>(new Task[] { taskA, taskB, taskC }, (tasks) =>tasks.Sum(t => (t as Task<int>).Result));
+            Console.WriteLine(sum);
         }
 
         private async static void PartialResponsive()
