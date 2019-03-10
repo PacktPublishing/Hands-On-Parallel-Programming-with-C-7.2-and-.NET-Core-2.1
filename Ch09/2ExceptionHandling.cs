4bfa3b48223247e66343ea257224fd3d90ce02af
shakti singh tanwar <shakti_stanwar@hotmail.com>
2019-03-11 02:52:31 +0530

    Chapter 6-14
    Chapter 6-14

---
+++ Ch09/2ExceptionHandling.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch09
+{
+    class _2ExceptionHandling
+    {
+        //public static void Main()
+        //{
+        //    // AsyncReturningValueExample();
+        //    Console.WriteLine("Main Method Startes");
+        //    // var task = Scenario1CallAsyncWithoutAwaitFromInsideTryCatch();
+        //    var task = Scenario2CallAsyncWithoutAwaitFromInsideTryCatch();
+        //    if (task.IsFaulted)
+        //        Console.WriteLine(task.Exception.Flatten().Message.ToString());
+        //    Console.WriteLine("In Main Method After calling method");
+        //    Console.ReadLine();
+        //}
+
+        public static void Main()
+        {
+            Console.WriteLine("Main Method Startes");
+            //var task = Scenario1CallAsyncWithoutAwaitFromOutsideTryCatch();
+            //if (task.IsFaulted)
+            //    Console.WriteLine(task.Exception.Flatten().Message.ToString());
+
+            //var task = Scenario2CallAsyncWithoutAwaitFromInsideTryCatch();
+            //if (task.IsFaulted)
+            //    Console.WriteLine(task.Exception.Flatten().Message.ToString());
+
+            //var task = Scenario3CallAsyncWithAwaitFromOutsideTryCatch();
+            //if (task.IsFaulted)
+            //    Console.WriteLine(task.Exception.Flatten().Message.ToString());
+
+
+            Scenario4CallAsyncWithoutAwaitFromOutsideTryCatch();
+
+            Console.WriteLine("In Main Method After calling method");
+            Console.ReadLine();
+        }
+
+        private async static void Scenario4CallAsyncWithoutAwaitFromOutsideTryCatch()
+        {
+            Task task = DoSomethingFaulty();
+            Console.WriteLine("This should not execute");          
+        }
+
+        private async static Task Scenario3CallAsyncWithAwaitFromOutsideTryCatch()
+        {
+            await DoSomethingFaulty();
+            Console.WriteLine("This should not execute");           
+        }
+        private async static Task Scenario2CallAsyncWithoutAwaitFromInsideTryCatch()
+        {
+            try
+            {
+                var task = DoSomethingFaulty();
+                Console.WriteLine("This should not execute");
+                task.ContinueWith((s) =>
+                {
+                    Console.WriteLine(s);
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        private async static Task Scenario2AsyncReturningTaskExample()
+        {
+            try
+            {
+                Task task = DoSomethingFaulty();
+                Console.WriteLine("This should not execute");
+                task.ContinueWith((s) =>
+                {
+                    Console.WriteLine(s);
+                });
+                // Console.WriteLine(data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        private async static Task Scenario1CallAsyncWithoutAwaitFromOutsideTryCatch()
+        {
+            Task task = DoSomethingFaulty();
+            Console.WriteLine("This should not execute");
+            try
+            {
+                task.ContinueWith((s) =>
+                {
+                    Console.WriteLine(s);
+                });
+                // Console.WriteLine(data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private async static void AsyncReturningValueExample()
+        {
+            try
+            {
+                string data = await GetDummyData();
+                Console.WriteLine(data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+          
+        }
+
+        private static Task DoSomethingFaulty()
+        {
+            Task.Delay(2000);
+            throw new Exception("This is custom exception.");
+        }
+
+        private static Task<string> GetDummyData()
+        {
+            Task.Delay(2000);
+            throw new Exception("This is custom exception.");
+        }
+
+    }
+}
